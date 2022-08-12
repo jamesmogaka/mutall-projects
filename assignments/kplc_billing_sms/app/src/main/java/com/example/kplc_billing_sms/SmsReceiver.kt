f6c8c3f5f7958c67462d98286b6071994ee72079
james mogaka <jamesoyondi23@gmail.com>
2022-08-12 10:49:36 +0300

    Improvement on sms receiver and update of events minutes

--- assignments/kplc_billing_sms/app/src/main/java/com/example/kplc_billing_sms/SmsReceiver.kt
+++ assignments/kplc_billing_sms/app/src/main/java/com/example/kplc_billing_sms/SmsReceiver.kt
@@ -4,24 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
-import android.util.Log
+
 
 
 class SmsReceiver:BroadcastReceiver() {
-    companion object{
-        private val TAG by lazy { SmsReceiver:: class.java.simpleName }
-    }
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        ///Check if the broadcast from the android system is about an sms received
+
+        //Check if the broadcast from the android system is about an sms received
         //If not exit the function
-        if ( !intent?.action.equals( Telephony.Sms.Intents.SMS_RECEIVED_ACTION ) ) return
+        if (!intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) return
 
         //Extract the message from the intent passed by the android system
         val extractMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
 
-        //????? specific address messages
+        //Iterate over the messages and print he originating address and message body
+        extractMessages.forEach { smsMessage ->
 
-        //Iterate over the messages and log them to the console
-        extractMessages.forEach {smsMessage -> Log.v( TAG, smsMessage.displayMessageBody ) }
+            //Filter the intents based on the originating address
+            //Check the origin of the message
+            if( smsMessage.displayOriginatingAddress == "97771" ) {
+                println("${smsMessage.displayOriginatingAddress} -> ${smsMessage.displayMessageBody}")
+            } else return
+        }
     }
 }
